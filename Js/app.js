701654e49feca9fb48d01c6eae32d2ed357ce0fa
Cici-4 <Cecilia_Rosales96@hotmail.com>
2023-04-17 21:55:58 -0300

    first commit

---
+++ Js/app.js
@@ -0,0 +1,34 @@
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modalContainer");
+
+
+let carrito = [];
+
+productos.forEach((product)=>{
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML =`
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="price">${(product.precio)}$</p>
+    `;
+ shopContent.append(content);
+ let comprar = document.createElement("button");
+ comprar.innerText = "comprar";
+ comprar.className = "comprar";
+
+ content.append(comprar);
+
+ comprar.addEventListener("click", ()=>{
+    carrito.push({
+        id: product.id,
+        img: product.img,
+        nombre: product.nombre,
+        precio: product.precio,
+    });
+    console.log(carrito);
+ });
+
+});
+
